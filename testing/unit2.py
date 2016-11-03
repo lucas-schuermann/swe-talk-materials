2665737e51023088a7fcdb137420878cdd22be84
Lucas Schuermann <lucas.v.schuermann@gmail.com>
2016-11-03 19:58:03 -0400

    another unit test

---
+++ testing/unit2.py
@@ -0,0 +1,2 @@
+def palindrome(test_string):
+	return (test_string[::-1])==test_string
---
+++ testing/unit2_test.py
@@ -0,0 +1,6 @@
+from unit2 import palindrome
+
+
+def test_palindrome():
+    assert palindrome('test') == 0
+    assert palindrome('kayak') == 1
